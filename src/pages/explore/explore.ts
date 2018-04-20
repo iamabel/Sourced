1ca63075216d01ebca76a2825261969aa2530fa0
Armando Belardo <armandoubelardo@gmail.com>
2018-04-20 16:55:38 -0400

    Fix bad merge, and naming errors

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -69,11 +69,11 @@ export class MapPage {
   // RANDOM, NAIVE DISTANCE COMPUTATION!
   getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
     var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1);
     var a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
       Math.sin(dLon/2) * Math.sin(dLon/2)
       ;
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
