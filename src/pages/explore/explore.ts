2721b67243d1c9e5d6679cdeab82234ce335e57c
Armando Belardo <armandoubelardo@gmail.com>
2018-04-20 16:44:53 -0400

    Adding Places data

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,9 +1,12 @@
 import { Component, ElementRef, ViewChild } from "@angular/core/";
-import { CameraPosition, GoogleMaps, GoogleMap, LatLng } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
 import { Geolocation } from '@ionic-native/geolocation';
 
+import {BottomDrawer} from '../../components/bottom-drawer/bottom-drawer';
 
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'explore-page',
@@ -11,7 +14,9 @@ import { NavController } from 'ionic-angular';
 })
 export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('drawer') drawer: BottomDrawer;
   map: GoogleMap;
+  places: Array<{name: string, photo: any}> = [];
 
   constructor(public navCtrl: NavController, private _geoLoc: Geolocation) {
   }
@@ -20,19 +25,31 @@ export class MapPage {
     this.initMap();
 
     this._geoLoc.getCurrentPosition().then( position => {
-      console.log(position.coords.longitude + ' ' + position.coords.latitude);
-      var loc = new LatLng(position.coords.latitude, position.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: position.coords.latitude, lng: position.coords.longitude},
+        zoom: 15
+      });
 
-      this.moveCamera(loc)
+      var service = new google.maps.places.PlacesService(this.map);
+      service.nearbySearch({
+        location: {lat: position.coords.latitude, lng: position.coords.longitude},
+        radius: 1000,
+        type: ['restaurant']
+      }, (results,status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      });
     }).catch(err => {
-      console.log(err);
+      console.log(err.toString());
     });
   }
 
   initMap() {
     let element = this.mapElement.nativeElement;
     this.map = GoogleMaps.create(element);
-    console.log("~Map it up~");
   }
 
   onSearch(ev: any) {
@@ -40,12 +57,31 @@ export class MapPage {
     console.log(val);
   }
 
-  moveCamera(loc: LatLng){
-    let options: CameraPosition = {
-      target: loc,
-      zoom: 15,
-      tilt: 10
-    }
-    this.map.moveCamera(options);
+  createMarker(place: any) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: this.map,
+      position: placeLoc,
+    });
+    this.places.push({name: place.name, photo: place.photos[0]});
+  }
+
+  // RANDOM, NAIVE DISTANCE COMPUTATION!
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
   }
 }
