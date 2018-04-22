b0d335d2aef28c6143861289f73ad9f512475f45
zcharris <zcharris@colgate.edu>
2018-04-22 16:45:34 -0400

    login done, background image needs fixing b/c it shows up on every page right now

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../pages/login/login';
+import { ChatPage } from '../../pages/chat/chat';
+import { MapPage } from '../../pages/explore/explore';
 
 @Component({
   selector: 'login-page',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
+  chatPage = ChatPage;
+  mapPage = MapPage;
   constructor(public navCtrl: NavController) {
 
   }
